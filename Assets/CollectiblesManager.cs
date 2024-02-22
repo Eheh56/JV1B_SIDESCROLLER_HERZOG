a4581167806b8c4f0e8bb5e7d874ae28dc5e674b
Eheh56 <julienherzog@orange.fr>
2024-02-22 18:58:53 +0100

    initialize

---
+++ Assets/CollectiblesManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro;
+
+public class CollectiblesManager : MonoBehaviour
+{
+    public TextMeshProUGUI gemUI;
+    int numGemsCollected = 0;
+
+    // Example of another collectible type
+    //public TextMeshProUGUI diamondUI;
+    //int numDiamondsCollected = 0;
+
+    private void OnEnable()
+    {
+        Gem.OnGemCollected += GemCollected;
+        //Diamond.OnDiamondCollected += DiamondCollected;
+    }
+
+    private void OnDisable()
+    {
+        Gem.OnGemCollected -= GemCollected;
+        //Diamond.OnDiamondCollected -= DiamondCollected;
+    }
+
+    private void GemCollected()
+    {
+        numGemsCollected++;
+        gemUI.text = numGemsCollected.ToString();
+    }
+
+    //private void DiamondCollected()
+    //{
+    //    numDiamondsCollected++;
+    //    diamondUI.text = numDiamondsCollected.ToString();
+    //}
+    public interface ICollectible
+    {
+        public void Collect();
+    }
+}
---
+++ Assets/Gem.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public interface ICollectible
+{
+    public void Collect();
+}
+
+
+public class Gem : MonoBehaviour, ICollectible
+{
+    public static event Action OnGemCollected;
+
+    // Example of using particles
+    //public ParticleSystem collectParticle;
+
+    public void Collect()
+    {
+        OnGemCollected?.Invoke();
+
+        // This is where you can play sounds or spawn particles
+        //Instantiate(collectParticle, transform.position, Quaternion.identity);
+
+        Debug.Log("Gem collected!");
+        Destroy(gameObject);
+    }
+
+}
